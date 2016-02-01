2584eab439336b1a4f03c67775658747ffe4407c
jes708 <jonschwarz@nyu.edu>
2016-01-31 22:15:18 -0500

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,44 @@
 # Implement your object-oriented solution here!
+
+# 1. take a maximum number
+# 2. Find the sum of all numbers 1^2 to max^2
+# 3. Square the sum of all numbers
+# 4. Subtract 3.) from 2.)
+
+class SumSquareDifference
+
+  def initialize(number)
+    @number = number
+  end
+
+  def sum_of_squares
+    
+    result = 0
+    i = 1
+    while i <= @number
+      result += (i ** 2) 
+      i += 1
+    end
+
+    result
+  end
+
+  def square_of_sums
+    
+    result = 0
+    i = 1
+    while i <= @number
+      result += i
+      i += 1
+    end
+
+    result ** 2
+  end
+
+  def difference
+
+    square_of_sums - sum_of_squares
+
+  end
+
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,36 @@
 # Implement your procedural solution here!
+
+# 1. take a maximum number
+# 2. Find the sum of all numbers 1^2 to max^2
+# 3. Square the sum of all numbers
+# 4. Subtract 3.) from 2.)
+
+def sum_of_squares(number)
+  
+  result = 0
+  i = 1
+  while i <= number
+    result += (i ** 2) 
+    i += 1
+  end
+
+  result
+end
+
+def square_of_sums(number)
+  
+  result = 0
+  i = 1
+  while i <= number
+    result += i
+    i += 1
+  end
+
+  result ** 2
+end
+
+def sum_square_difference(number)
+
+  square_of_sums(number) - sum_of_squares(number)
+
+end
